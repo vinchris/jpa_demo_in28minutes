07161990501845a82ffedb550b7ce9ea22fc4e4d
Chris <ateamcristian@yahoo.de>
2023-01-12 10:36:37 +0200

    initial commit for demo jpa project

---
+++ src/test/java/com/in28minutes/jpa/hibernate/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.jpa.hibernate.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
