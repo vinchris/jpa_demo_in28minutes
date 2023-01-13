a263b8f32b152c7deed1715a902f285530969d05
Chris <ateamcristian@yahoo.de>
2023-01-13 11:22:56 +0200

    extended the data model with table/entity student, passport and review

--- src/main/java/com/in28minutes/jpa/hibernate/demo/entity/Review.java
+++ src/main/java/com/in28minutes/jpa/hibernate/demo/entity/Review.java
@@ -1,4 +1,30 @@
 package com.in28minutes.jpa.hibernate.demo.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
 public class Review {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String rating;
+
+    private String description;
+
+    public Review(String rating, String description) {
+        this.rating = rating;
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Review [%s %s]", rating, description);
+    }
 }
--- src/main/java/com/in28minutes/jpa/hibernate/demo/entity/Student.java
+++ src/main/java/com/in28minutes/jpa/hibernate/demo/entity/Student.java
@@ -1,10 +1,29 @@
 package com.in28minutes.jpa.hibernate.demo.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
 public class Student {
 
+    @Id
+    @GeneratedValue
     private Long id;
 
+    @Column(nullable = false)
     private String name;
 
-    private Passport passport;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    protected Student() {
+    }
+
+    @Override
+    public String toString(){
+        return String.format("Student [%s]", name);
+    }
 }
