68e2dbaad49fe8f142bd6cd439625aeb253651eb
Chris <ateamcristian@yahoo.de>
2023-01-16 15:30:35 +0200

    finished working with different types of inheritance types

--- src/main/java/com/in28minutes/jpa/hibernate/demo/entityWithInheritanceHierarchies/Employee.java
+++ src/main/java/com/in28minutes/jpa/hibernate/demo/entityWithInheritanceHierarchies/Employee.java
@@ -9,9 +9,10 @@ import lombok.ToString;
 @Data
 @ToString
 @NoArgsConstructor
-//@Inheritance(strategy = InheritanceType.SINGLE_TABLE) // this will create a single table EMPLOYEE with table rows (with null values respectively) for FullTimeEmployee and PartTimeEmployee
-//@DiscriminatorColumn(name = "EmployeeType") // default is DTYPE, the one that distinguishes between different types of extended entities
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS) // creates a table per concrete entity class, the root entity class Employee should be abstract in order to not create a useless empty table
+//@Inheritance(strategy = InheritanceType.SINGLE_TABLE) // -- this will create a single table EMPLOYEE with table rows (with null values respectively) for FullTimeEmployee and PartTimeEmployee
+//@DiscriminatorColumn(name = "EmployeeType") // -- default is DTYPE, the one that distinguishes between different types of extended entities
+//@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS) // -- creates a table per concrete entity class, the root entity class Employee should be abstract in order to not create a useless empty table
+@Inheritance(strategy = InheritanceType.JOINED) // -- A strategy in which fields that are specific to a subclass are mapped to a separate table than the fields that are common to the parent class, and a join is performed to instantiate the subclass.
 public abstract class Employee {
 
     @Id
